10924e0878ffc265144c1b4ff98be60fed7156ad
Alex The JPEG <alexthejpeg@gmail.com>
2023-05-30 17:22:48 -0400

    docs(ping): add "!" to `/ping` description

--- src/ted_cogs/ping.py
+++ src/ted_cogs/ping.py
@@ -5,7 +5,7 @@ class Ping(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
     
-    @discord.slash_command(name="ping", description="Ping Ted")
+    @discord.slash_command(name="ping", description="Ping Ted!")
     async def ping(self, ctx: discord.ApplicationContext) -> None:
         await ctx.respond(f"🏓 Pong! (**{round(self.bot.latency * 1000, 1)} ms**)")
 
